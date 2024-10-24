a10cedba1d7bd9b1bedcc53279e117b359051551
Вячеслав Лукин <bafana.slv@gmail.com>
2024-10-24 16:03:37 +0300

    Init project.

---
+++ retailing/urls.py
@@ -0,0 +1,16 @@
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1",
+        description="API управления торговой сетью электроники.",
+        terms_of_service="http://localhost:8000/retailing/",
+        contact=openapi.Contact(email="foxship@yandex.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
