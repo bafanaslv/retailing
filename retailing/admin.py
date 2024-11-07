73a431a5814449c649be0b43a48f7551476e4275
Вячеслав Лукин <bafana.slv@gmail.com>
2024-11-07 06:58:47 +0300

    Create admin-panel.

--- retailing/admin.py
+++ retailing/admin.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.contrib import admin
 
 from retailing.models import Supplier, Payable
@@ -18,8 +20,20 @@ class SupplierAdmin(admin.ModelAdmin):
         "created_at"
     )
     list_filter = ('city',)
+    search_fields = ("name",)
+    search_help_text = "Поиск по названию"
 
 
 @admin.register(Payable)
 class PayableAdmin(admin.ModelAdmin):
     list_display = ("owner", "supplier", "amount", "created_at", "is_paid", "paid_date")
+
+    actions = ["clear_payable"]
+
+    def clear_payable(self, request, queryset):
+        """Админ-действие для погашения задолженности на 0 для выбранных объектов.
+        - queryset: набор объектов, к которым применяется действие.
+        """
+        queryset.update(amount=0, is_paid=True, paid_date=date.today)
+
+    clear_payable.short_description = "Погасить задолженность перед поставщиком"
